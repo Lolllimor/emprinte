49910514340b9fd3c65ae8ed669a416506f5bab0
Lollimor <etap-09@etap-09s-MacBook-Air.local>
2025-11-07 18:12:28 +0100

    fix: max width fix

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -4,113 +4,118 @@ import { RiInstagramFill } from 'react-icons/ri';
 
 export function Footer() {
   return (
-    <footer className="w-full bg-white px-4 py-9 xl:px-[120px] lg:px-[64px] md:px-[32px] flex flex-col items-start gap-2.5">
-      <div className="w-full flex flex-col items-end gap-[42px]">
-        <div className="flex items-start gap-[38px] md:justify-between w-full">
-          <div className="w-[224px] flex flex-col items-start gap-6">
-            <Logo />
+    <footer className="w-full bg-white px-4 py-9 xl:px-[120px] lg:px-[64px] md:px-[32px]  ">
+      <div className="max-w-[1440px] mx-auto  flex flex-col items-start gap-2.5">
+        <div className="w-full flex flex-col items-end gap-[42px]">
+          <div className="flex items-start gap-[38px] md:justify-between w-full">
+            <div className="w-[224px] flex flex-col items-start gap-6">
+              <Logo />
 
-            <div className="w-full flex flex-col justify-center items-start gap-[30px]">
-              <div className="w-full">
-                <div className="text-base leading-[28px] font-bold text-black mb-1 font-lora">
-                  Email
+              <div className="w-full flex flex-col justify-center items-start gap-[30px]">
+                <div className="w-full">
+                  <div className="text-base leading-[28px] font-bold text-black mb-1 font-lora">
+                    Email
+                  </div>
+                  <div className="text-base leading-[28px] font-semibold text-[#7B7B7B] font-campton">
+                    hello@emprintereaders.com
+                  </div>
                 </div>
-                <div className="text-base leading-[28px] font-semibold text-[#7B7B7B] font-campton">
-                  hello@emprintereaders.com
+
+                <div className="w-full">
+                  <div className="text-base leading-[28px] font-bold text-black mb-2 font-lora">
+                    Phone
+                  </div>
+                  <div className="flex flex-col items-start gap-2">
+                    <div className="text-base leading-[28px] font-semibold text-[#7B7B7B] font-campton">
+                      Adepeju - 081029348475
+                    </div>
+                    <div className="text-base leading-[28px] font-semibold text-[#7B7B7B] font-campton">
+                      Abiola - 081029348475
+                    </div>
+                  </div>
                 </div>
               </div>
+            </div>
 
-              <div className="w-full">
-                <div className="text-base leading-[28px] font-bold text-black mb-2 font-lora">
-                  Phone
+            <div className="flex items-start gap-[64px]">
+              <div className="md:flex hidden flex-col items-start gap-6">
+                <p className="text-[28px] font-bold font-lora text-black">
+                  Navigation
+                </p>
+                <div className="flex flex-col gap-6 font-[20px] text-[#2B2742]">
+                  <a
+                    href="/"
+                    className="text-base leading-[28px]  text-[#7B7B7B] font-campton"
+                  >
+                    Home
+                  </a>
+                  <a
+                    href="/"
+                    className="text-base leading-[28px]  text-[#7B7B7B] font-campton"
+                  >
+                    Bootcamps
+                  </a>
+                  <a
+                    href="/"
+                    className="text-base leading-[28px]  text-[#7B7B7B] font-campton"
+                  >
+                    Initiatives
+                  </a>
+                  <a
+                    href="/"
+                    className="text-base leading-[28px]  text-[#7B7B7B] font-campton"
+                  >
+                    About Us
+                  </a>
                 </div>
-                <div className="flex flex-col items-start gap-2">
-                  <div className="text-base leading-[28px] font-semibold text-[#7B7B7B] font-campton">
-                    Adepeju - 081029348475
-                  </div>
-                  <div className="text-base leading-[28px] font-semibold text-[#7B7B7B] font-campton">
-                    Abiola - 081029348475
-                  </div>
+              </div>
+              <div className="flex flex-col items-center gap-2">
+                <div className="text-base md:text-[28px] leading-[28px] font-bold text-center text-black font-lora">
+                  Follow Us
+                </div>
+
+                <div className="inline-flex flex-col justify-center items-start gap-6">
+                  <a
+                    href="https://instagram.com/emprinte"
+                    aria-label="Instagram"
+                  >
+                    <RiInstagramFill color="#015B51" size={32} />
+                  </a>
+
+                  <a
+                    href="https://linkedin.com/company/emprinte"
+                    className="w-8 h-8 rounded bg-[#015B51] flex items-center justify-center"
+                    aria-label="LinkedIn"
+                  >
+                    <ImLinkedin2 color="#fff" size={24} />
+                  </a>
+
+                  <a
+                    href="https://twitter.com/emprinte"
+                    className="w-8 h-8 rounded bg-[#015B51] flex items-center justify-center"
+                    aria-label="Twitter"
+                  >
+                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none">
+                      <path
+                        d="M32 25.5025C32 29.0762 29.0762 32 25.5025 32H6.49746C2.92383 32 0 29.0762 0 25.5025V6.49746C0 2.9239 2.92383 0 6.49746 0H25.5025C29.0762 0 32 2.9239 32 6.49746V25.5025Z"
+                        fill="#015B51"
+                      />
+                      <path
+                        d="M18.3946 14.4043L26.092 4.81055H23.8624L17.2802 13.0151L10.6972 4.81055H3.34766L13.605 17.5952L5.90696 27.1897H8.13653L14.7195 18.9845L21.3031 27.1897H28.6527L18.3946 14.4043ZM6.972 6.54961H9.86244L25.0277 25.4505H22.1372L6.972 6.54961Z"
+                        fill="#F0F0F1"
+                      />
+                    </svg>
+                  </a>
                 </div>
               </div>
             </div>
           </div>
 
-          <div className="flex items-start gap-[64px]">
-            <div className="md:flex hidden flex-col items-start gap-6">
-              <p className="text-[28px] font-bold font-lora text-black">
-                Navigation
-              </p>
-              <div className="flex flex-col gap-6 font-[20px] text-[#2B2742]">
-                <a
-                  href="/"
-                  className="text-base leading-[28px]  text-[#7B7B7B] font-campton"
-                >
-                  Home
-                </a>
-                <a
-                  href="/"
-                  className="text-base leading-[28px]  text-[#7B7B7B] font-campton"
-                >
-                  Bootcamps
-                </a>
-                <a
-                  href="/"
-                  className="text-base leading-[28px]  text-[#7B7B7B] font-campton"
-                >
-                  Initiatives
-                </a>
-                <a
-                  href="/"
-                  className="text-base leading-[28px]  text-[#7B7B7B] font-campton"
-                >
-                  About Us
-                </a>
-              </div>
+          <div className="flex flex-col items-start gap-2 w-full">
+            <div className="w-full h-px bg-[#015B51]"></div>
+            <div className="text-xs leading-[28px] font-semibold text-[#7B7B7B] font-campton">
+              @2025 Emprinte Readers Hub All Right Reserved
             </div>
-            <div className="flex flex-col items-center gap-2">
-              <div className="text-base md:text-[28px] leading-[28px] font-bold text-center text-black font-lora">
-                Follow Us
-              </div>
-
-              <div className="inline-flex flex-col justify-center items-start gap-6">
-                <a href="https://instagram.com/emprinte" aria-label="Instagram">
-                  <RiInstagramFill color="#015B51" size={32} />
-                </a>
-
-                <a
-                  href="https://linkedin.com/company/emprinte"
-                  className="w-8 h-8 rounded bg-[#015B51] flex items-center justify-center"
-                  aria-label="LinkedIn"
-                >
-                  <ImLinkedin2 color="#fff" size={24} />
-                </a>
-
-                <a
-                  href="https://twitter.com/emprinte"
-                  className="w-8 h-8 rounded bg-[#015B51] flex items-center justify-center"
-                  aria-label="Twitter"
-                >
-                  <svg width="32" height="32" viewBox="0 0 32 32" fill="none">
-                    <path
-                      d="M32 25.5025C32 29.0762 29.0762 32 25.5025 32H6.49746C2.92383 32 0 29.0762 0 25.5025V6.49746C0 2.9239 2.92383 0 6.49746 0H25.5025C29.0762 0 32 2.9239 32 6.49746V25.5025Z"
-                      fill="#015B51"
-                    />
-                    <path
-                      d="M18.3946 14.4043L26.092 4.81055H23.8624L17.2802 13.0151L10.6972 4.81055H3.34766L13.605 17.5952L5.90696 27.1897H8.13653L14.7195 18.9845L21.3031 27.1897H28.6527L18.3946 14.4043ZM6.972 6.54961H9.86244L25.0277 25.4505H22.1372L6.972 6.54961Z"
-                      fill="#F0F0F1"
-                    />
-                  </svg>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="flex flex-col items-start gap-2 w-full">
-          <div className="w-full h-px bg-[#015B51]"></div>
-          <div className="text-xs leading-[28px] font-semibold text-[#7B7B7B] font-campton">
-            @2025 Emprinte Readers Hub All Right Reserved
           </div>
         </div>
       </div>
--- src/components/sections/Initiatives.tsx
+++ src/components/sections/Initiatives.tsx
@@ -1,7 +1,10 @@
 export function Initiatives() {
   return (
-    <section id="initiatives" className="w-full bg-[#F0FFFD] px-6 py-8 xl:pt-[133px] lg:pt-[90px] md:pt-[80px] pt-[60px] lg:px-[75px] xl:px-[120px] flex flex-col items-start gap-7">
-      <div className="w-full flex flex-col  md:flex-row md:justify-between md:items-end lg:items-end xl:items-center items-start gap-4">
+    <section
+      id="initiatives"
+      className="w-full bg-[#F0FFFD] px-6 py-8 xl:pt-[133px] lg:pt-[90px] md:pt-[80px] pt-[60px] lg:px-[75px] xl:px-[120px] flex flex-col items-start gap-7"
+    >
+      <div className="w-full flex flex-col  md:flex-row md:justify-between md:items-end lg:items-end xl:items-center items-start gap-4  max-w-[1440px] mx-auto">
         <div className="flex flex-col gap-4  md:max-w-[500px]">
           <div className="flex h-6 px-4 justify-center items-center gap-2.5 rounded-3xl bg-[#E63715] w-fit">
             <span className="text-xs md:text-base leading-[150%] font-normal text-white font-campton">
