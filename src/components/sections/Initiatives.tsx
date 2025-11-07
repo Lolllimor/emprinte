6329e7afc867e03f5384cfcfc60bf26d345b06c9
Lollimor <etap-09@etap-09s-MacBook-Air.local>
2025-11-07 16:48:15 +0100

    feat: addition on navgition logic

--- src/components/sections/Initiatives.tsx
+++ src/components/sections/Initiatives.tsx
@@ -1,6 +1,6 @@
 export function Initiatives() {
   return (
-    <section className="w-full bg-[#F0FFFD] px-6 py-8 xl:pt-[133px] lg:pt-[90px] md:pt-[80px] pt-[60px] lg:px-[75px] xl:px-[120px] flex flex-col items-start gap-7">
+    <section id="initiatives" className="w-full bg-[#F0FFFD] px-6 py-8 xl:pt-[133px] lg:pt-[90px] md:pt-[80px] pt-[60px] lg:px-[75px] xl:px-[120px] flex flex-col items-start gap-7">
       <div className="w-full flex flex-col  md:flex-row md:justify-between md:items-end lg:items-end xl:items-center items-start gap-4">
         <div className="flex flex-col gap-4  md:max-w-[500px]">
           <div className="flex h-6 px-4 justify-center items-center gap-2.5 rounded-3xl bg-[#E63715] w-fit">
